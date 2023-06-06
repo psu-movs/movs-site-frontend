7578f1f9340299f9f102e649d1e62a7579694ce8
Damego <danyabatueff@gmail.com>
2023-06-06 21:04:28 +0500

    feat: add montserrat font

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -1,14 +1,4 @@
-// Fonts Example
-import { Inter } from 'next/font/google';
+import { Inter as InterFont, Montserrat as MontserratFont } from 'next/font/google';
 
-const GoogleInterFont = Inter({ subsets: ['latin'] });
-
-export default GoogleInterFont;
-
-// Local Fonts example
-// more details here: https://nextjs.org/docs/app/building-your-application/optimizing/fonts#local-fonts
-// import localFont from 'next/font/local';
-
-// const LocalFont = localFont({src: [{path: './path-of-font-file-regular.woff', weight: '400', style: 'normal'}], fallback: ['Arial', 'sans-serif']})
-
-// export default LocalFont;
+export const Inter = InterFont({ subsets: ['cyrillic'] });
+export const MontserratSemiBold = MontserratFont({ subsets: ['cyrillic'], weight: '600' })
--- src/components/themeRegistry/theme.ts
+++ src/components/themeRegistry/theme.ts
@@ -2,22 +2,28 @@
 
 import { createTheme, ThemeOptions } from "@mui/material/styles";
 
-import GoogleInterFont from "@/app/fonts/fonts";
+import { Inter, MontserratSemiBold } from "@/app/fonts/fonts";
 
 // When needed::: first argument is needed if you have common enterprise theme, and second argument is to override your enterprise theme.
 // apply fonts to all other typography options like headings, subtitles, etc...
 const defaultTheme = createTheme(
   {
     typography: {
-      fontFamily: GoogleInterFont.style.fontFamily,
-      body1: { fontFamily: GoogleInterFont.style.fontFamily },
-      body2: { fontFamily: GoogleInterFont.style.fontFamily },
+      fontFamily: Inter.style.fontFamily,
+      body1: { fontFamily: Inter.style.fontFamily },
+      body2: { fontFamily: Inter.style.fontFamily },
+      h1: { fontFamily: MontserratSemiBold.style.fontFamily, fontWeight: MontserratSemiBold.style.fontWeight },
+      h2: { fontFamily: MontserratSemiBold.style.fontFamily, fontWeight: MontserratSemiBold.style.fontWeight },
+      h3: { fontFamily: MontserratSemiBold.style.fontFamily, fontWeight: MontserratSemiBold.style.fontWeight },
+      h4: { fontFamily: MontserratSemiBold.style.fontFamily, fontWeight: MontserratSemiBold.style.fontWeight },
+      h5: { fontFamily: MontserratSemiBold.style.fontFamily, fontWeight: MontserratSemiBold.style.fontWeight },
+      h6: { fontFamily: MontserratSemiBold.style.fontFamily, fontWeight: MontserratSemiBold.style.fontWeight },
     },
     palette: {
       primary: {
-        main: '#2148C0'
-      }
-    }
+        main: "#2148C0",
+      },
+    },
   },
   {} satisfies ThemeOptions
 );
