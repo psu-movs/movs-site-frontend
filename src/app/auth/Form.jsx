c5e5f1c486273eaa40dab22788ace6ce96abcd21
Damego <danyabatueff@gmail.com>
2023-06-06 21:04:49 +0500

    refactor: auth form

--- src/app/auth/Form.jsx
+++ src/app/auth/Form.jsx
@@ -1,29 +1,50 @@
-"use client"
+"use client";
 
-import {Button, Container, Link, Stack, TextField, Checkbox, FormControlLabel, FormGroup} from "@mui/material";
+import {
+  Button,
+  Container,
+  Link,
+  Stack,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  FormGroup, Typography
+} from "@mui/material";
 
 export default function Form() {
+  return (
+    <Container maxWidth={"xs"}>
+      <Stack spacing={5}>
+        <img src={"/logo.svg"} alt={"logo"} width={"60%"} />
 
-    return (
-        <Container maxWidth={'sm'}>
-            <Stack spacing={5}>
-                <img src={"/logo.svg"} alt={'logo'} width={"60%"}/>
+        <Typography variant="h4" >Добро пожаловать</Typography>
 
-                <h1>Добро пожаловать</h1>
+        <TextField id="email" label="Электронная почта" variant="outlined" sx={{backgroundColor: '#FFFFFF'}} />
+        <TextField
+          id="password"
+          label="Пароль"
+          variant="outlined"
+          type={"password"}
+          sx={{backgroundColor: '#FFFFFF'}}
+        />
 
-                <TextField id="email" label="Электронная почта" variant="outlined" />
-                <TextField id="password" label="Пароль" variant="outlined" type={"password"} />
+        <Stack
+          direction="row"
+          spacing={10}
+          sx={{ alignItems: "center", justifyContent: "space-between" }}
+        >
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked size={"medium"}/>}
+              label={<Typography sx={{fontWeight: '600'}}>Запомнить меня</Typography>}
+            />
+          </FormGroup>
 
-                <Stack direction="row" spacing={10}>
-                    <FormGroup>
-                        <FormControlLabel control={<Checkbox defaultChecked/>} label="Запомнить меня" />
-                    </FormGroup>
+          <Link href={"#"}><Typography sx={{fontWeight: '600'}}>Забыли пароль?</Typography></Link>
+        </Stack>
 
-                    <Link href={"#"}>Забыли пароль?</Link>
-                </Stack>
-
-                <Button variant="contained">Войти</Button>
-            </Stack>
-        </Container>
-    )
+        <Button variant="contained" size={"large"} sx={{boxShadow: 0}}>Войти</Button>
+      </Stack>
+    </Container>
+  );
 }
