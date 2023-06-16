1fbd390334832a90446c3f389093682ce4320836
Damego <danyabatueff@gmail.com>
2023-06-16 18:33:45 +0500

    refactor: decompose manager; make page as server component

---
+++ src/app/manager/Manager.tsx
@@ -0,0 +1,17 @@
+import ControlPanel from "@/app/manager/ControlPanel";
+import Divider from "@mui/material/Divider";
+import NewsContainer from "@/app/manager/NewsContainer";
+import DepartmentContainer from "@/app/manager/DepartmentContainer";
+import { Container } from "@mui/material";
+
+export default function Manager({user, active}) {
+
+  return (
+    <Container maxWidth={"lg"}>
+      <ControlPanel user={user} />
+      <Divider />
+      {active === "news" && <NewsContainer />}
+      {active === "department" && <DepartmentContainer />}
+    </Container>
+  )
+}
--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -1,48 +1,23 @@
-"use client";
+import { redirect } from 'next/navigation'
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Container } from "@mui/material";
-import Divider from "@mui/material/Divider";
-import ControlPanel from "@/app/manager/ControlPanel";
-import { ClientUser } from "@/http/responseModels";
 import httpClient from "@/http";
-import NewsContainer from "@/app/manager/NewsContainer";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import DepartmentContainer from "@/app/manager/DepartmentContainer";
 
-export default function ManagerPage() {
-  const { push } = useRouter();
-  const searchParams = useSearchParams();
+import Manager from "@/app/manager/Manager";
+
+export default async function ManagerPage({ searchParams }) {
   const active = searchParams.get("active");
+  const response = await httpClient.getMe();
 
-  const [user, setUser] = useState<ClientUser>();
-
-  const fetchUser = async () => {
-    const response = await httpClient.getMe();
-    if (!response || (response && response.error)) {
-      push("/login");
-    }
-    if (response.permissions === 0) {
-      push("/");
-    }
-    setUser(response);
-  };
-
-  useEffect(() => {
-    fetchUser();
-  }, []);
-
-  if (!user) return;
+  if (!response || (response && response.error)) {
+    redirect("/login");
+  }
+  if (response.permissions === 0) {
+    redirect("/");
+  }
 
   return (
     <main>
-      <Container maxWidth={"lg"}>
-        <ControlPanel user={user} />
-        <Divider />
-        {active === "news" && <NewsContainer />}
-        {active === "department" && <DepartmentContainer />}
-      </Container>
+      <Manager user={response} active={active} />
     </main>
   );
 }
