e56b45fd8b64803ef1b90417766e3921d77b3771
Damego <danyabatueff@gmail.com>
2023-06-19 10:34:01 +0500

    refactor: move science work modal to own component

--- src/app/science_works/components/ScienceWorkBlock.tsx
+++ src/app/science_works/components/ScienceWorkBlock.tsx
@@ -1,70 +1,88 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-import { 
-  Stack, 
-  Modal,  
-  Typography, 
-  Card, 
-  CardActions, 
-  CardContent, 
-  CardMedia, 
-  Button } from "@mui/material";
+import {
+  Stack,
+  Modal,
+  Typography,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+} from "@mui/material";
 import { ScienceWork } from "@/http/responseModels";
 
 interface ScienceWorkProps {
- work: ScienceWork;
+  work: ScienceWork;
 }
 
-const ScienceWorkBlock = ({work}: ScienceWorkProps) =>  {
+const ScienceWorkModal = ({
+  work,
+  isOpened,
+  onClose,
+}: {
+  work: ScienceWork;
+  isOpened: boolean;
+  onClose(): void;
+}) => (
+  <Modal open={isOpened} onClose={onClose}>
+    <Stack
+      spacing={1}
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        minWidth: 300,
+        maxWidth: 800,
+        minHeight: 300,
+        maxHeight: 800,
+        backgroundColor: "#FFF",
+        padding: "1%",
+        overflow: "auto",
+      }}
+    >
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        {work.title}
+      </Typography>
+      <Typography textAlign={"justify"}>{work.description}</Typography>
+    </Stack>
+  </Modal>
+);
+
+const ScienceWorkBlock = ({ work }: ScienceWorkProps) => {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <Card sx={{ maxWidth: 345, minWidth: 300 }}>
+    <Card sx={{ maxWidth: 345, minWidth: 300, marginBottom: '2%' }}>
       <CardMedia
         sx={{ height: 140 }}
         image={work.image_url}
         title={work.title}
       />
+
       <CardContent>
         <Typography gutterBottom variant="h5">
           {work.title}
         </Typography>
       </CardContent>
+
       <CardActions>
-        <Button size="small" variant="contained" onClick={() => setOpen(true)}>Подробнее</Button>
+        <Button size="small" variant="contained" onClick={() => setOpen(true)}>
+          Подробнее
+        </Button>
       </CardActions>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-      >
-        <Stack
-          spacing={1}
-          sx={{
-            position: 'absolute' as 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            minWidth: 300,
-            maxWidth: 800,
-            minHeight: 300,
-            maxHeight: 800,
-            bgcolor: "#F2F4F8",
-            border: '1px solid #2148C0',
-            padding: "1%",
-            overflow: 'auto',
-          }}
-        >
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {work.title}
-          </Typography>
-          <Typography textAlign={"justify"}>
-            {work.description}
-          </Typography>
-        </Stack>
-      </Modal>
+
+      <ScienceWorkModal
+        work={work}
+        isOpened={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      />
     </Card>
   );
-}
+};
 
 export default ScienceWorkBlock;
