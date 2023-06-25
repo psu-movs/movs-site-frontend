d74322b3104bbd7f3e70872af425c143ba7bb7f8
Damego <danyabatueff@gmail.com>
2023-06-25 18:48:40 +0500

    refactor!: comment test

--- src/components/VocationalTest.jsx
+++ src/components/VocationalTest.jsx
@@ -21,29 +21,29 @@ export default function VocationalTest() {
           justifyContent="space-between"
           sx={{ padding: isPhone ? "3%" : isTablet ? "5%" : "7%" }}
         >
-          <Box
-            sx={{
-              backgroundColor: "rgba(0, 0, 0, 0.12)",
-            }}
-            border={1}
-            borderColor={"#FFFFFF"}
-            width={"205px"}
-            height="35px"
-            borderRadius="40px"
-          >
-            <Stack
-              sx={{
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100%",
-              }}
-            >
-              <Typography color={"#ffffff"} fontSize={10} variant={"body1"}>
-                ПРОФОРИЕНТАЦИОННЫЙ ТЕСТ
-              </Typography>
-            </Stack>
-          </Box>
+          {/*<Box*/}
+          {/*  sx={{*/}
+          {/*    backgroundColor: "rgba(0, 0, 0, 0.12)",*/}
+          {/*  }}*/}
+          {/*  border={1}*/}
+          {/*  borderColor={"#FFFFFF"}*/}
+          {/*  width={"205px"}*/}
+          {/*  height="35px"*/}
+          {/*  borderRadius="40px"*/}
+          {/*>*/}
+          {/*  <Stack*/}
+          {/*    sx={{*/}
+          {/*      justifyContent: "center",*/}
+          {/*      alignItems: "center",*/}
+          {/*      width: "100%",*/}
+          {/*      height: "100%",*/}
+          {/*    }}*/}
+          {/*  >*/}
+          {/*    <Typography color={"#ffffff"} fontSize={10} variant={"body1"}>*/}
+          {/*      ПРОФОРИЕНТАЦИОННЫЙ ТЕСТ*/}
+          {/*    </Typography>*/}
+          {/*  </Stack>*/}
+          {/*</Box>*/}
           <Box maxWidth={"sm"}>
             {!isPhone ? (
               <Typography
@@ -57,30 +57,30 @@ export default function VocationalTest() {
               <></>
             )}
           </Box>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#fd5600",
-              width: isPhone ? "180px" : "220px",
-              height: isPhone ? "40px" : "55px",
-              marginTop: isTablet ? "3%" : "5%",
-            }}
-          >
-            Пройти
-          </Button>
-          {!isPhone ? (
-            <Typography
-              variant={"body1"}
-              fontSize={16}
-              color={"#ffffff"}
-              paddingTop={"4%"}
-            >
-              Пройдите небольшой тест, чтобы узнать подходит ли вам данная
-              кафедра.
-            </Typography>
-          ) : (
-            <></>
-          )}
+          {/*<Button*/}
+          {/*  variant="contained"*/}
+          {/*  sx={{*/}
+          {/*    backgroundColor: "#fd5600",*/}
+          {/*    width: isPhone ? "180px" : "220px",*/}
+          {/*    height: isPhone ? "40px" : "55px",*/}
+          {/*    marginTop: isTablet ? "3%" : "5%",*/}
+          {/*  }}*/}
+          {/*>*/}
+          {/*  Пройти*/}
+          {/*</Button>*/}
+          {/*{!isPhone ? (*/}
+          {/*  <Typography*/}
+          {/*    variant={"body1"}*/}
+          {/*    fontSize={16}*/}
+          {/*    color={"#ffffff"}*/}
+          {/*    paddingTop={"4%"}*/}
+          {/*  >*/}
+          {/*    Пройдите небольшой тест, чтобы узнать подходит ли вам данная*/}
+          {/*    кафедра.*/}
+          {/*  </Typography>*/}
+          {/*) : (*/}
+          {/*  <></>*/}
+          {/*)}*/}
         </Stack>
       </Stack>
     </Container>
